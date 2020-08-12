06deda465f0ca4e5e5c7e7c9cd24a00c4d735295
iona-b <ionabrabender@gmail.com>
2020-08-12 09:45:08 -0700

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,20 @@
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+      let min = i;
+      for (let j = i + 1; j < len; j++) {
+          if (arr[min] > arr[j]) {
+              min = j;
+          }
+      }
+      if (min !== i) {
+          let tmp = arr[i];
+          arr[i] = arr[min];
+          arr[min] = tmp;
+      }
+  }
+  return arr;
 }
+
+array = [5, 6, 4, 8, 9, 3, 1, 2, 7]
+selectionSort(array)
