9fd0e7626349940f3f41f7a7950fe21bc8b29839
darrickpang <dkypang@yahoo.com>
2020-08-12 10:30:45 -0700

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,11 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for(let x = 0; x < arr.length; x++){
+    for(let y = x; y > 0 && arr[y - 1] > arr[y]; y--){
+      let temp = arr[y]
+      arr[y] = arr[y - 1]
+      arr[y - 1] = temp
+    }
+  }
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,15 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for(let x = 0; x < arr.length - 1; x++){
+    let yMin = x 
+    for(let y = x + 1; y < arr.length; y++){
+      if(arr[y] < arr[yMin]){
+        yMin = y
+      }
+    }
+    let temp = arr[x]
+    arr[x] = arr[yMin]
+    arr[y] = temp
+  }
 }
